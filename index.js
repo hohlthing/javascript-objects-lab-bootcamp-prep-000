5a4bf4b5699c49f1ff4c0501cd1568d8de360c1b
michael hohl <mhohl@rocketmail.com>
2018-03-08 19:36:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,18 +5,14 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
-
   return object
 }
 function deleteFromObjectByKey(object, key) {
   const newObj = Object.assign({}, object)
-
   delete newObj[key]
-
   return newObj
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
-
   return object
 }
